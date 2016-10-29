cf53e3371352f00dcbac5bf682dc3c1ee309b506
Thomas Rivas <rivassi.thomas@gmail.com>
2016-10-29 15:35:59 -0700

    add abiity to add new questions to the board

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,6 +7,12 @@ export default Ember.Route.extend({
   },
 
   actions: {
+    saveQuestion3(params){
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    },
+
     destroyQuestion(question) {
       question.destroyQuestion();
       this.transitionTo('index');
