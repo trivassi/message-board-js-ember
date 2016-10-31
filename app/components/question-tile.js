4ab0bfff05b7a13c52d6402f58916b3118271cb8
Thomas Rivas <rivassi.thomas@gmail.com>
2016-10-30 23:40:53 -0700

    add header with links and in tile add links to each question

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,11 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    delete(question) {
-      if (confirm('Are you sure you want to delete this question? ')) {
-        this.sendAction('destroyQuestion', question);
-      }
-    }
-  }
+  // updateQuestionForm: false,
+  //
+  // actions: {
+  //   update(question, params) {
+  //     this.sendAction('update', question, params);
+  //   },
+  //   delete(question) {
+  //     if (confirm('Are you sure you want to delete this question? ')) {
+  //       this.sendAction('destroyQuestion', question);
+  //     }
+  //   }
+  // }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -13,9 +13,19 @@ export default Ember.Route.extend({
       this.transitionTo('index');
     },
 
-    destroyQuestion(question) {
-      question.destroyQuestion();
-      this.transitionTo('index');
-    }
+    // update(question, params){
+    //   Object.keys(params).forEach(function(key) {
+    //     if(params[key]!==undefined) {
+    //       question.set(key,params[key]);
+    //     }
+    //   });
+    //   question.save();
+    //   this.transitionTo('index');
+    // },
+    //
+    // destroyQuestion(question) {
+    //   question.destroyQuestion();
+    //   this.transitionTo('index');
+    // }
   }
 });
