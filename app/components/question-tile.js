c55389a82c6573910981dc030ebca45d739b18d5
Thomas Rivas <rivassi.thomas@gmail.com>
2016-11-06 22:44:03 -0800

    add jumbotron, navbar and styling

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // heading: Ember.computed('question.content','question.author', function(){
+  //   return this.get('question.content') + ' - ' + this.get('question.author');
+  // })
 
 });
