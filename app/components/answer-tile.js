c10bcd44b64b2475279bc651adbddaa7e608339c
Thomas Rivas <rivassi.thomas@gmail.com>
2016-11-07 07:57:34 -0800

    add fave route and fave answers service

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  faveAnswer: Ember.inject.service(),
+
   actions: {
+    addToFave(answer){
+      this.get('faveAnswer').add(answer);
+    },
     delete(answer) {
       if (confirm('Are you sure you want to delete this answer?')) {
         this.sendAction('destroyAnswer', answer);
